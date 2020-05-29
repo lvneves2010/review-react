14f5b9bbddc6cb6ea2533fcf3a41669b9f8ff50d
Leonardo <lvneves2010@gmail.com>
2020-05-29 17:16:25 -0300

    Cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Primeiro from './components / basicos/Primeiro'
 import ComParametros from './components / basicos/ComParametros'
 import Fragmentos from './components / basicos/Fragmentos'
 import Aleatorio from './components / basicos/Aleatorio'
+import Card from './components /layout/Card'
 
 
 export default _ => 
         <div>
             <h1>Fundamentos React refactored</h1>
-            <Aleatorio min={1} max={60} />        
-            <Fragmentos />
-            <ComParametros
+            <Card titulo="#04 - Palpites Mega Sena">
+                <Aleatorio min={1} max={60} />        
+            </Card>
+            <Card titulo="#03 - Fragmentos">
+                <Fragmentos />      
+            </Card>
+            <Card titulo="#02 - Com Parametros">
+                <ComParametros
                 titulo="Situação do aluno"
                 aluno="Bruno"
                 nota={9.3} />
@@ -19,5 +25,8 @@ export default _ =>
                 titulo="Situação do aluno"
                 aluno="Gustavo"
                 nota={9.9} />
-            <Primeiro />
+            </Card>
+            <Card titulo="#01 - Primeiro">
+                <Primeiro />
+            </Card>
         </div>
