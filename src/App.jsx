bb0795610dde3d27a21c1897fef49d1cae0d22e7
Leonardo <lvneves2010@gmail.com>
2020-06-02 08:45:27 -0300

    children

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 
 import Primeiro from './components / basicos/Primeiro'
@@ -5,28 +6,39 @@ import ComParametros from './components / basicos/ComParametros'
 import Fragmentos from './components / basicos/Fragmentos'
 import Aleatorio from './components / basicos/Aleatorio'
 import Card from './components /layout/Card'
+import Familia from './components / basicos/Familia'
+import FamiliaMembro from './components / basicos/FamiliaMembro'
 
 
 export default _ => 
-        <div>
+        <div className="App" >
             <h1>Fundamentos React refactored</h1>
-            <Card titulo="#04 - Palpites Mega Sena">
-                <Aleatorio min={1} max={60} />        
-            </Card>
-            <Card titulo="#03 - Fragmentos">
-                <Fragmentos />      
-            </Card>
-            <Card titulo="#02 - Com Parametros">
-                <ComParametros
-                titulo="Situação do aluno"
-                aluno="Bruno"
-                nota={9.3} />
-                <ComParametros
-                titulo="Situação do aluno"
-                aluno="Gustavo"
-                nota={9.9} />
-            </Card>
-            <Card titulo="#01 - Primeiro">
-                <Primeiro />
-            </Card>
+            <div className="Cards">
+                <Card titulo="#05 - componente com filhos" color="#7FDBFF" >
+                    <Familia sobrenome="Neves" >
+                        <FamiliaMembro nome="Leonardo" />
+                        <FamiliaMembro nome="Bruno" />
+                        <FamiliaMembro nome="Gustavo" />
+                    </Familia>       
+                </Card>
+                <Card titulo="#04 - Palpites Mega Sena" color="#00E" >
+                    <Aleatorio min={1} max={60} />        
+                </Card>
+                <Card titulo="#03 - Fragmentos" color="#00E" >
+                    <Fragmentos />      
+                </Card>
+                <Card titulo="#02 - Com Parametros" color="#00E" >
+                    <ComParametros
+                    titulo="Situação do aluno"
+                    aluno="Bruno"
+                    nota={9.3} />
+                    <ComParametros
+                    titulo="Situação do aluno"
+                    aluno="Gustavo"
+                    nota={9.9} />
+                </Card>
+                <Card titulo="#01 - Primeiro" color="#00E" >
+                    <Primeiro />
+                </Card>
+            </div>
         </div>
